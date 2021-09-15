f48a24cf681d292352a10fda7b4674ae5f6a382e
masum_fahim <masum_fahim@nidaansystems.com>
2021-09-14 22:37:47 -0700

    GRPC Added

--- service1/server.js
+++ service1/server.js
@@ -3,6 +3,32 @@
 
 const express = require('express');
 
+
+var PROTO_PATH = __dirname + '/protos/hello.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).hello;
+
+
+function GetStatusReport(call, callback) {
+  callback(null, {isActive: 'Hello ' + call.request.name});
+}
+var server = new grpc.Server();
+server.addService(hello_proto.StatusReport.service, {GetStatusReport: GetStatusReport});
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+  console.log('GRPC Server Started');
+});
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
--- service2/server.js
+++ service2/server.js
@@ -3,8 +3,29 @@
 
 const express = require('express');
 
+var PROTO_PATH = __dirname + '/protos/helloworld.proto';
+
+var packageDefinition = protoLoader.loadSync(
+  PROTO_PATH,
+  {keepCase: true,
+   longs: String,
+   enums: String,
+   defaults: true,
+   oneofs: true
+  });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).hello;
+
+var client = new hello_proto.Greeter(target, grpc.credentials.createInsecure());
+
+client.HelloRequest({name: user}, function(err, response) {
+  console.log('Greeting:', response.isActive);
+});
+
+
+
+
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 // App
@@ -12,6 +33,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World from service 2');
 });
+
+app.get('/service2', (req, res) => {
+  res.send('ami service2 bolchi');
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
